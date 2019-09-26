5eab5794ce5592ebe785782d5f94f3d18a01f2a7
Pedro Severo <phsevero94@gmail.com>
2019-09-26 14:11:23 -0300

    Desafio tarde - PASSO 1

---
+++ semana-3/quinta/script.js
@@ -0,0 +1,7 @@
+function cadastrarDespesa() {
+    document.getElementById("valor-da-despesa").value = "";
+    document.getElementById("tipo-da-despesa").value = "";
+    document.getElementById("descrição-da-despesa").value = "";
+
+}
+
