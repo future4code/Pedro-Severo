59b85896d6258ae03eec1c928f96438c308abda0
Pedro Severo <phsevero94@gmail.com>
2019-09-26 17:57:04 -0300

    passo 6 e 7

--- semana-3/quinta/script.js
+++ semana-3/quinta/script.js
@@ -6,7 +6,7 @@ class Despesa {
     }
 
     formatarDespesa () {
-        const despesaFormatada = "<p> R$" + this.valor + "</p>" +
+        const despesaFormatada = "<p class='valor-unitario'> R$ " + this.valor + ",00 </p>" +
                                  "<p>" + this.tipo + "</p>" +
                                  "<p>" + this.descricao + "</p><hr>";
         return despesaFormatada;
@@ -53,3 +53,51 @@ function formatarTodasAsDespesas() {
         listaDeDespesas.innerHTML += despesaAtual.formatarDespesa(); 
     }
 }
+
+function filtrar () {
+    let tipoDaDespesa = document.getElementById("escolha-do-tipo-da-despesa").value;
+    let valorMinimo = document.getElementById("valor-minimo-da-despesa").value;
+    let valorMaximo = document.getElementById("valor-maximo-da-despesa").value;
+
+    if (tipoDaDespesa !== "") {
+        const filtroPeloTipo = despesas.filter(despesaFiltradaTipo => {
+            return despesaFiltradaTipo.tipo === tipoDaDespesa
+        })
+        const listaDeDespesasTipo = document.getElementById("lista-de-despesa");
+        listaDeDespesasTipo.innerHTML = "";
+
+        for(let despesaAtualTipo of filtroPeloTipo) {
+            listaDeDespesasTipo.innerHTML += despesaAtualTipo.formatarDespesa(); 
+        }
+    } else if (valorMinimo !== "") {
+        const filtroPeloValorMinimo = despesas.filter(despesaFiltradaValorMin => {
+            return despesaFiltradaValorMin.valor >= valorMinimo 
+        })
+        const listaDeDespesasValorMinimo = document.getElementById("lista-de-despesa");
+        listaDeDespesasValorMinimo.innerHTML = "";
+
+        for(let despesaAtualValorMin of filtroPeloValorMinimo) {
+            listaDeDespesasValorMinimo.innerHTML += despesaAtualValorMin.formatarDespesa();
+        }
+
+    } else if (valorMaximo !== "") {
+        const filtroPeloValorMaximo = despesas.filter(despesaFiltradaValorMax => {
+            return despesaFiltradaValorMax.valor <= valorMaximo 
+        })
+        const listaDeDespesasValorMaximo = document.getElementById("lista-de-despesa");
+        listaDeDespesasValorMaximo.innerHTML = "";
+
+        for(let despesaAtualValorMax of filtroPeloValorMaximo) {
+            listaDeDespesasValorMaximo.innerHTML += despesaAtualValorMax.formatarDespesa();
+    }
+    }
+}
+
+// function somarValores {
+    
+    
+    
+//     let valorDoExtrato = document.getElementById("valor-total").value;
+//     valorDoExtrato.innerHTML =
+    
+// }
