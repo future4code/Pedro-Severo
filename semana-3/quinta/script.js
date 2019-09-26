92856a4ca6261271ae5b20adad53369d6b2f8d65
Pedro Severo <phsevero94@gmail.com>
2019-09-26 14:31:49 -0300

    passo 2

--- semana-3/quinta/script.js
+++ semana-3/quinta/script.js
@@ -1,7 +1,26 @@
-function cadastrarDespesa() {
-    document.getElementById("valor-da-despesa").value = "";
-    document.getElementById("tipo-da-despesa").value = "";
-    document.getElementById("descrição-da-despesa").value = "";
+class Despesa {
+    constructor (valor, tipo, descricao) {
+        this.valor = valor;
+        this.tipo = tipo;
+        this.descricao = descricao;
+    }
 
+    formatarDespesa () {
+
+    }
+}
+
+const despesas = [];
+
+function gerarDespesa() {
+    let valor = document.getElementById("valor-da-despesa").value;
+    let tipo = document.getElementById("tipo-da-despesa").value;
+    let descricao = document.getElementById("descrição-da-despesa").value;
+
+    let despesa = new Despesa (valor, tipo, descricao);
+
+    despesas.push(despesa);
+
+    console.log(despesas);
 }
 
