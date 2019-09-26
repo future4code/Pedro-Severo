4930eaabd06d0d714d8789ea294d90b4fedd79ac
Pedro Severo <phsevero94@gmail.com>
2019-09-26 14:47:40 -0300

    passo 3

--- semana-3/quinta/script.js
+++ semana-3/quinta/script.js
@@ -17,10 +17,12 @@ function gerarDespesa() {
     let tipo = document.getElementById("tipo-da-despesa").value;
     let descricao = document.getElementById("descrição-da-despesa").value;
 
-    let despesa = new Despesa (valor, tipo, descricao);
-
-    despesas.push(despesa);
-
-    console.log(despesas);
+    if (valor === "" || tipo === "" || descricao === "") {
+        alert("Todos os campos devem estar preenchidos");
+    } else {
+        let despesa = new Despesa (valor, tipo, descricao);
+        despesas.push(despesa);
+        console.log(despesas);
+    }
 }
 
