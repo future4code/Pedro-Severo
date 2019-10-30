57815289f03601b2949dae7424ad229c70c1b1db
Pedro Severo <phsevero94@gmail.com>
2019-10-30 18:42:37 -0300

    ainda com problema na função dispatch

--- semana-9/terça/missao-newton-react-boilerplate/src/reducers/tasks.js
+++ semana-9/terça/missao-newton-react-boilerplate/src/reducers/tasks.js
@@ -1,14 +1,19 @@
 import { ADD_TODO } from './actionTypes'
 
 const initialState = [{
-    text: "dsadasdasds",
+    id: 0,
+    text: "dsadasdasds"
 }]
 
 const tasks = (state = initialState, action) => {
     console.log(action)
     switch (action.type) {
-	case ADD_TODO:
-		return { ...state, inicialText: action.playload.text }
+    case ADD_TODO:
+        const newTask = {
+            id: Date.now,
+            text: action.playload.text,
+        }
+		return [ ...state, newTask ]
 	default:
 		return state 
     }
