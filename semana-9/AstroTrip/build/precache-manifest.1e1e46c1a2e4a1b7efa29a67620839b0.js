17131046b99ae37396c7f93dce233467b55388aa
Pedro Severo <phsevero94@gmail.com>
2019-11-05 16:48:38 -0300

    último comeńtário

---
+++ semana-9/AstroTrip/build/precache-manifest.1e1e46c1a2e4a1b7efa29a67620839b0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49adb640395f1fac4f7a7e1854df21b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37c4c275abc204b2b60c",
+    "url": "/static/js/2.84ad52d5.chunk.js"
+  },
+  {
+    "revision": "07b1841d9b3672bb28f6",
+    "url": "/static/js/main.72bf6ab3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
