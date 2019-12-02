2dbb1bb1c0a8c4df04a7f08df4a66177c69626d6
Pedro Severo <phsevero94@gmail.com>
2019-12-02 16:31:00 -0300

    validando

--- semana-13/Exercicios-Node/Exercicio-1/myCalculator.js
+++ semana-13/Exercicios-Node/Exercicio-1/myCalculator.js
@@ -2,12 +2,19 @@ const operation = process.argv[2];
 const firstNumber = Number(process.argv[3]);
 const secondNumber = Number(process.argv[4]);
 
-if (operation === "add") {
-    console.log(`Resposta: ${firstNumber + secondNumber}`);
-} else if (operation === "sub") {
-    console.log(`Resposta: ${firstNumber - secondNumber}`);
-} else if (operation === "mult") {
-    console.log(`Resposta: ${firstNumber * secondNumber}`);
-} else if (operation === "div") {
-    console.log(`Resposta: ${firstNumber / secondNumber}`);
-}
+
+if (firstNumber === false || secondNumber === false) {
+    console.log("É necessário colocar um número válido.");
+} else if (operation !== "add" || operation !== "sub" || operation !== "mult" || operation !== "div") {
+    console.log("É necessário colocar uma operação matemática válida.");
+} else {
+    if (operation === "add") {
+        console.log(`Resposta: ${firstNumber + secondNumber}`);
+    } else if (operation === "sub") {
+        console.log(`Resposta: ${firstNumber - secondNumber}`);
+    } else if (operation === "mult") {
+        console.log(`Resposta: ${firstNumber * secondNumber}`);
+    } else if (operation === "div") {
+        console.log(`Resposta: ${firstNumber / secondNumber}`);
+    }
+};
