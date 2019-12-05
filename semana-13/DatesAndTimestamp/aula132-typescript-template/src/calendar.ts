e2ff5f1c837196e5b326182c4b1a4a86d1c80626
Pedro Severo <phsevero94@gmail.com>
2019-12-05 14:38:34 -0300

    até o passo 2, pronto

--- semana-13/DatesAndTimestamp/aula132-typescript-template/src/calendar.ts
+++ semana-13/DatesAndTimestamp/aula132-typescript-template/src/calendar.ts
@@ -1,2 +1,23 @@
 import * as moment from "moment";
+import { readFile } from 'fs';
 
+
+type event = {
+    name: string,
+    description: string, 
+    date: Date,
+};
+
+const events: string = "events.json";
+
+const getEvents = (err: Error, data:Buffer) => {
+    if(err){
+        console.error(err);
+        return;
+    }
+
+    const eventsJSONContent:string = data.toString();
+    console.log("Arquivo lido com sucesso:", eventsJSONContent);
+}
+
+readFile(events, getEvents);
