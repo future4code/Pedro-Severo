e60180d1b16d496eb075c895c1e30de56a33a548
Pedro Severo <phsevero94@gmail.com>
2019-12-05 13:20:58 -0300

    Dates and Timestamp

---
+++ semana-13/DatesAndTimestamp/exercicios/build/exercicio1.js
@@ -0,0 +1,6 @@
+const constantinopla = new Date("1453-05-29");
+const revFrancesa = new Date("1789-07-14");
+const republica = new Date("1889-11-15");
+const niverGoli = new Date("1996-08-15");
+console.log(constantinopla.getTime(), revFrancesa.getTime(), republica.getTime(), niverGoli.getTime());
+//# sourceMappingURL=exercicio1.js.map
---
+++ semana-13/DatesAndTimestamp/exercicios/src/exercicio1.ts
@@ -0,0 +1,10 @@
+const constantinopla = new Date("1453-05-29");
+const revFrancesa = new Date("1789-07-14");
+const republica = new Date("1889-11-15");
+const niverGoli = new Date("1996-08-15");
+console.log(
+  constantinopla.getTime(),
+  revFrancesa.getTime(),
+  republica.getTime(),
+  niverGoli.getTime()
+);
