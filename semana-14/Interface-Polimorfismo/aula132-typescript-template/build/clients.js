8082a158effc52c432543faa2a02b2ec21aca4f9
Pedro Severo <phsevero94@gmail.com>
2019-12-11 16:27:22 -0300

    basic done

--- semana-14/Interface-Polimorfismo/aula132-typescript-template/build/clients.js
+++ semana-14/Interface-Polimorfismo/aula132-typescript-template/build/clients.js
@@ -4,31 +4,42 @@ const clientsTypes_1 = require("./clientsTypes");
 const clientsTypes_2 = require("./clientsTypes");
 const clientsTypes_3 = require("./clientsTypes");
 class ResidentialClient extends clientsTypes_3.Residence {
-    constructor(name, cpf, cep, clientNumber) {
-        super(name, cpf, cep);
-        this.clientNumber = clientNumber;
+    constructor() {
+        super(...arguments);
+        this.clientName = this.name;
+        this.clientNumber = Math.floor(Math.random() * 1000);
+        this.consumedEnergy = Math.floor(Math.random() * 10000) * 0.75;
     }
-    ;
     calculateBill() {
-        return 2;
+        return this.consumedEnergy;
     }
     ;
 }
 exports.ResidentialClient = ResidentialClient;
 ;
-const newClient = new ResidentialClient("Pedro", "cpf", "cep", 1);
-console.log(newClient.clientNumber);
 class ComercialClient extends clientsTypes_2.Trade {
+    constructor() {
+        super(...arguments);
+        this.clientName = this.tradeName;
+        this.clientNumber = Math.floor(Math.random() * 1000);
+        this.consumedEnergy = Math.floor(Math.random() * 10000) * 0.53;
+    }
     calculateBill() {
-        return 2;
+        return this.consumedEnergy;
     }
     ;
 }
 exports.ComercialClient = ComercialClient;
 ;
 class IndustrialClient extends clientsTypes_1.Industry {
+    constructor() {
+        super(...arguments);
+        this.clientName = this.industryName;
+        this.clientNumber = Math.floor(Math.random() * 1000);
+        this.consumedEnergy = Math.floor(Math.random() * 10000) * 0.45 + 10000;
+    }
     calculateBill() {
-        return 2;
+        return this.consumedEnergy;
     }
     ;
 }
@@ -47,7 +58,40 @@ class ClientManager {
         return this.clients.length;
     }
     ;
+    printClientBills(clientName, clientNumber, clients) {
+        for (let client of clients) {
+            if (clientName === client.clientName && clientNumber === client.clientNumber) {
+                console.log(`${client.clientNumber} - ${client.clientName} - R$${client.consumedEnergy}`);
+                return;
+            }
+            ;
+        }
+        ;
+        console.log("Nome do cliente e/ou número informado está(ao) errado(s)");
+    }
+    ;
+    calculateAllIncome(clients) {
+        let allIncome = 0;
+        for (let consumedEnergy of clients) {
+            allIncome += consumedEnergy.consumedEnergy;
+        }
+        ;
+        return allIncome;
+    }
+    ;
 }
 exports.ClientManager = ClientManager;
 ;
+const newResidentialClient = new ResidentialClient("Pedro", "cpf do Pedro", "cep");
+const newComercialClient = new ComercialClient("Future4", "cnpj da Future4", "cep");
+const newIndustrialClient = new IndustrialClient("Indústria", "cnpj da Insdústria", "cep");
+const allClients = [];
+const clientManager = new ClientManager(allClients);
+clientManager.addClient(newResidentialClient);
+clientManager.addClient(newComercialClient);
+clientManager.addClient(newIndustrialClient);
+console.log(allClients);
+console.log(clientManager.getClientsQuantity());
+console.log(clientManager.printClientBills("Pedro", newResidentialClient.clientNumber, allClients));
+console.log(clientManager.calculateAllIncome(allClients));
 //# sourceMappingURL=clients.js.map
--- semana-14/Interface-Polimorfismo/aula132-typescript-template/src/clients.ts
+++ semana-14/Interface-Polimorfismo/aula132-typescript-template/src/clients.ts
@@ -5,31 +5,33 @@ import { Residence } from './clientsTypes';
 
 export class ResidentialClient extends Residence implements Client {
     clientName: string = this.name;
-    clientNumber: number;  
-    consumedEnergy: number;
+    clientNumber: number = Math.floor(Math.random()*1000);  
+    consumedEnergy: number = Math.floor(Math.random()*10000)*0.75;
   
     calculateBill(): number {
-      return 2
+        return this.consumedEnergy
     };
+
 };
+
 
 export class ComercialClient extends Trade implements Client {
     clientName: string = this.tradeName;
-    clientNumber: number;  
-    consumedEnergy: number;
+    clientNumber: number = Math.floor(Math.random()*1000);  
+    consumedEnergy: number = Math.floor(Math.random()*10000)*0.53;
 
     calculateBill(): number {
-        return 2
+        return this.consumedEnergy
     };
 };
 
 export class IndustrialClient extends Industry implements Client {
     clientName: string = this.industryName;
-    clientNumber: number;  
-    consumedEnergy: number;
+    clientNumber: number = Math.floor(Math.random()*1000);  
+    consumedEnergy: number = Math.floor(Math.random()*10000)*0.45+10000;
 
     calculateBill(): number {
-        return 2
+        return this.consumedEnergy
     };
 };
 
@@ -66,3 +68,22 @@ export class ClientManager {
         return allIncome;
     };
 };
+
+const newResidentialClient: ResidentialClient = new ResidentialClient("Pedro", "cpf do Pedro", "cep");
+const newComercialClient: ComercialClient = new ComercialClient("Future4", "cnpj da Future4", "cep");
+const newIndustrialClient: IndustrialClient = new IndustrialClient("Indústria", "cnpj da Insdústria", "cep");
+
+const allClients: Client[] = [];
+
+
+const clientManager: ClientManager = new ClientManager(allClients);
+clientManager.addClient(newResidentialClient);
+clientManager.addClient(newComercialClient);
+clientManager.addClient(newIndustrialClient);
+
+console.log(allClients);
+
+console.log(clientManager.getClientsQuantity());
+
+clientManager.printClientBills("Pedro", newResidentialClient.clientNumber, allClients);
+console.log(clientManager.calculateAllIncome(allClients))
