babdc9b21634685008518074a3e7fe35eb7ddcbf
Pedro Severo <phsevero94@gmail.com>
2020-01-06 15:14:17 -0300

    creating Cliente and Request entities

--- semana-16/F4Painting/src/business/entities/Client.ts
+++ semana-16/F4Painting/src/business/entities/Client.ts
@@ -1,9 +1,27 @@
+import { Request } from './Request';
+
 export class Client {
     constructor(
         private id: string,
         private name: string,
-        private email: string
+        private email: string,
+        private requests: Request[]
     ) {}
 
-    
-}
+    public getId() {
+        return this.id
+    };
+
+    public getName() {
+        return this.name
+    };
+
+    public getEmail() {
+        return this.email
+    };
+
+    public getRequests() {
+        return this.requests
+    };
+
+};
