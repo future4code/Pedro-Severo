484d45effba7dcd6ec82e400aa8e3517a7b8cde7
Pedro Severo <phsevero94@gmail.com>
2020-01-06 13:26:56 -0300

    f4painting

---
+++ semana-16/F4Painting/src/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import { AddressInfo } from 'net'
+import knex from 'knex';
+
+const app = express();
+app.use(express.json()); // Linha mágica (middleware)
+
+const connection = knex({
+  client: 'mysql',
+  connection: {
+    host : 'ec2-18-229-236-15.sa-east-1.compute.amazonaws.com',
+    user : 'USUARIO',
+    password : 'SENHA',
+    database : 'exercicios'
+  }
+});
+
+
+
+// Trecho do código responsável por inicializar todas as APIs
+const server = app.listen(process.env.PORT || 3000, () => {
+  if(server){
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});
