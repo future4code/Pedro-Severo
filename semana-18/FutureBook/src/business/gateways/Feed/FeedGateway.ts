eab1bd028697f35f595c469a4c8155c9d05ded72
Pedro Severo <phsevero94@gmail.com>
2020-01-23 14:48:58 -0300

    getFeed endpoint done

--- semana-18/FutureBook/src/business/gateways/Feed/FeedGateway.ts
+++ semana-18/FutureBook/src/business/gateways/Feed/FeedGateway.ts
@@ -1,5 +1,10 @@
 import { Post } from "../../entities/Post";
 
+export interface FeedResponse {
+    post: Post
+    name: string
+};
+
 export interface FeedGateway {
-    getPostsFeedForUser(userId: string): Promise<any>
+    getPostsFeedForUser(userId: string): Promise<FeedResponse[]>
 };
--- semana-18/FutureBook/src/data/Feed/FeedDatabase.ts
+++ semana-18/FutureBook/src/data/Feed/FeedDatabase.ts
@@ -1,15 +1,14 @@
 import { KnexConnection } from "../KnexConnection/knexConnection";
-import { FeedGateway } from "../../business/gateways/Feed/FeedGateway";
+import { FeedGateway, FeedResponse } from "../../business/gateways/Feed/FeedGateway";
 import { Post, PostType } from "../../business/entities/Post";
 
 interface PostFeedModel {
-    sender_id: string,
-    receptor_id: string,
     picture: string,
     description: string,
     type: PostType,
     user_id: string,
-    creation_date: string
+    creation_date: string,
+    userName: string
 };
 
 export class FeedDatabase extends KnexConnection implements FeedGateway {
@@ -17,19 +16,24 @@ export class FeedDatabase extends KnexConnection implements FeedGateway {
         super()
     };
 
-    async getPostsFeedForUser(userId: string): Promise<any> {
+    async getPostsFeedForUser(userId: string): Promise<FeedResponse[]> {
         const query = await this.connection.raw(
               `
-              SELECT * FROM friendships friends JOIN posts_fbook posts 
+              SELECT posts.picture, posts.description, posts.type, posts.user_id, posts.creation_date, users.name as userName 
+              FROM friendships friends 
+              JOIN posts_fbook posts 
               ON (friends.sender_id=posts.user_id) OR (friends.receptor_id=posts.user_id) 
-              WHERE (sender_id='${userId}') OR (receptor_id='${userId}');
+              JOIN users_fbook users 
+              ON (posts.user_id=users.id) OR (posts.user_id=users.id) 
+              WHERE (sender_id="${userId}") OR (receptor_id="${userId}");
               `
         );
 
         const postsFromDb: PostFeedModel[] = query[0]
 
         return postsFromDb.map(post => ({
-            post: new Post(post.picture, post.description, post.type, post.user_id, new Date(post.creation_date))
+            post: new Post(post.picture, post.description, post.type, post.user_id, new Date(post.creation_date)),
+            name: post.userName
         }))
     };
 };
